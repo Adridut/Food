439244f785628e8364b493880d2eb6ce8ff1734d
Adrien Dutfoy <dutfoy.adrien@hotmail.fr>
2021-06-15 17:11:31 +0200

    search bar

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,6 +1,12 @@
 import * as React from 'react';
 import { useState } from 'react';
-import { StyleSheet, ScrollView, Text, View, Image } from 'react-native';
+import { StyleSheet, ScrollView, Text, View, Image, TextInput } from 'react-native';
+
+import { Dimensions } from 'react-native';
+
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
+
 
 import Card from '../components/Card';
 
@@ -42,49 +48,61 @@ const DATA = [
 export default function TabOneScreen() {
 
   const [filter, setFilter] = useState('food');
+  const [search, setSearch] = useState('');
+
 
   return (
-    <View style={styles.container}>
+    <View style={styles.screen}>
       <Text style={styles.title}>Delicious food for you</Text>
-      <ScrollView horizontal={true} style={{ flexGrow: 0 }}>
-        <Text style={[filter == 'food' ? styles.selectedFilter : styles.filter]} onPress={() => setFilter('food')}>Foods</Text>
-        <Text style={[filter == 'drink' ? styles.selectedFilter : styles.filter]} onPress={() => setFilter('drink')}>Drinks</Text>
-        <Text style={[filter == 'snack' ? styles.selectedFilter : styles.filter]} onPress={() => setFilter('snack')}>Snacks</Text>
-        <Text style={[filter == 'sauce' ? styles.selectedFilter : styles.filter]} onPress={() => setFilter('sauce')}>Sauces</Text>
-      </ScrollView>
-      <ScrollView horizontal={true}>
-        {DATA.filter(value => value.type == filter).map((value, index) => {
-          return <Card name={value.name} price={value.price}></Card>
-        })}
-      </ScrollView>
-      <ScrollView horizontal={true}>
-        <Image style={{width: 30, height: 30}} source={require('../assets/images/home.png')}
-        />
-        <Image style={{width: 30, height: 30}} source={require('../assets/images/heart.png')}
-        />
-        <Image style={{width: 30, height: 30}} source={require('../assets/images/user.png')}
-        />
-        <Image style={{width: 30, height: 30}} source={require('../assets/images/history.png')}
-        />
-      </ScrollView>
+      <View style={styles.container}>
+        <View style={styles.searchContainer}>
+          <Image style={{ width: 20, height: 20, marginRight: 5 }} source={require('../assets/images/search.png')}
+          />
+          <TextInput
+            onChangeText={setSearch}
+            value={search} placeholder="Search"
+          ></TextInput>
+        </View>
+        <ScrollView horizontal={true} style={{ flexGrow: 0 }}>
+          <Text style={[filter == 'food' ? styles.selectedFilter : styles.filter]} onPress={() => setFilter('food')}>Foods</Text>
+          <Text style={[filter == 'drink' ? styles.selectedFilter : styles.filter]} onPress={() => setFilter('drink')}>Drinks</Text>
+          <Text style={[filter == 'snack' ? styles.selectedFilter : styles.filter]} onPress={() => setFilter('snack')}>Snacks</Text>
+          <Text style={[filter == 'sauce' ? styles.selectedFilter : styles.filter]} onPress={() => setFilter('sauce')}>Sauces</Text>
+        </ScrollView>
+        <ScrollView horizontal={true}>
+          {DATA.filter(value => value.type == filter && value.name.includes(search)).map((value, index) => {
+            return <Card name={value.name} price={value.price}></Card>
+          })}
+        </ScrollView>
+        <ScrollView horizontal={true}>
+          <Image style={{ width: 30, height: 30 }} source={require('../assets/images/home.png')}
+          />
+          <Image style={{ width: 30, height: 30 }} source={require('../assets/images/heart.png')}
+          />
+          <Image style={{ width: 30, height: 30 }} source={require('../assets/images/user.png')}
+          />
+          <Image style={{ width: 30, height: 30 }} source={require('../assets/images/history.png')}
+          />
+        </ScrollView>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  screen: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginTop: 30
+    marginTop: 50
   },
-  container2: {
+  container: {
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: 15
   },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
+    marginLeft: 15
   },
   separator: {
     marginVertical: 30,
@@ -101,4 +119,12 @@ const styles = StyleSheet.create({
     borderBottomColor: '#FA4A0C',
     borderBottomWidth: 3
   },
+  searchContainer: {
+    height: 40,
+    width: 30 + windowWidth / 2,
+    backgroundColor: '#EFEEEE',
+    borderRadius: 30,
+    padding: 10,
+    flexDirection: 'row'
+  }
 });
